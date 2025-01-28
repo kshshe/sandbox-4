68820538cea3a9c059b7a911ac830c4e4e8680f0
Aleksei Laud <mail@a-laud.ru>
2025-01-28 13:09:50 +0400

    Remove logging of buffer statistics.

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -3,7 +3,7 @@ const MAX_BUFFER_SIZE = 1_000_000;
 
 setInterval(() => {
     if (buffer.length < MAX_BUFFER_SIZE) {
-        for (let i = 0; i < 10000; i++) {
+        for (let i = 0; i < 10_000; i++) {
             buffer.push(Math.random());
         }
     }
@@ -11,13 +11,6 @@ setInterval(() => {
 
 let fromRandomCount = 0;
 let fromBufferCount = 0;
-
-setInterval(() => {
-    console.log(`Buffer size: ${buffer.length}`);
-    console.log(`From random: ${fromRandomCount} of ${fromRandomCount + fromBufferCount} (${Math.floor(fromRandomCount / (fromRandomCount + fromBufferCount) * 100)}%)`);
-    fromRandomCount = 0;
-    fromBufferCount = 0;
-}, 1000);
 
 export function random() {
     const fromBuffer = buffer.pop();
