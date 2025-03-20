2389aa14ddc0b23832f7481d58e438f65a275b94
Aleksei Laud <mail@a-laud.ru>
2025-03-20 21:48:44 +0400

    Add @vercel/analytics dependency and initialize it

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { startProcessing } from './forces';
 import { addConsoleApi } from './utils/addConsoleApi';
 import './ui/main'
 import 'react-tooltip/dist/react-tooltip.css'
+import { inject } from "@vercel/analytics"
 
 Points.init()
 
 void startProcessing();
 
 addConsoleApi();
+
+inject();
