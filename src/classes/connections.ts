5386bf6f27943d365c4849a4b7ef716c71c1608d
Aleksei Laud <mail@a-laud.ru>
2025-03-20 11:48:32 +0400

    Add mergeConnections to optimize save process

--- src/classes/connections.ts
+++ src/classes/connections.ts
@@ -11,7 +11,38 @@ export class Connections {
   private static connections: TConnection[] = Storage.get('Connections', []);
 
   static saveConnections(): void {
+    this.mergeConnections();
     Storage.set('Connections', this.connections);
+  }
+
+  static mergeConnections(): void {
+    let wasChanged = false;
+    for (const conn of this.connections) {
+      const toX = conn.to.x;
+      const toY = conn.to.y;
+
+      if (conn.to.x === conn.from.x && conn.to.y === conn.from.y) {
+        this.removeConnection(conn);
+        wasChanged = true;
+        continue;
+      }
+
+      for (const otherConn of this.connections) {
+        if (otherConn === conn) continue;
+
+        if (
+          otherConn.from.x === toX && otherConn.from.y === toY
+        ) {
+          conn.to = otherConn.to;
+          this.removeConnection(otherConn);
+          wasChanged = true;
+          break;
+        }
+      }
+    }
+    if (wasChanged) {
+      this.saveConnections();
+    }
   }
 
   static addConnection(connection: TConnection): void {
@@ -21,11 +52,11 @@ export class Connections {
 
   static removeConnection(connection: TConnection): void {
     this.connections = this.connections.filter(
-      c => !(c.from.x === connection.from.x && 
-             c.from.y === connection.from.y && 
-             c.to.x === connection.to.x && 
-             c.to.y === connection.to.y &&
-             c.type === connection.type)
+      c => !(c.from.x === connection.from.x &&
+        c.from.y === connection.from.y &&
+        c.to.x === connection.to.x &&
+        c.to.y === connection.to.y &&
+        c.type === connection.type)
     );
     this.saveConnections();
   }
@@ -33,7 +64,7 @@ export class Connections {
   static getConnectionFromOrTo(coordinates: TCoordinate): TConnection[] {
     return this.connections.filter(
       c => c.from.x === coordinates.x && c.from.y === coordinates.y ||
-           c.to.x === coordinates.x && c.to.y === coordinates.y
+        c.to.x === coordinates.x && c.to.y === coordinates.y
     );
   }
 
