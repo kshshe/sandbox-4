661314c9da18d36353f646c74fa98856fbaa36f7
Aleksei Laud <mail@a-laud.ru>
2025-03-25 13:47:17 +0400

    Add default carrying chance for Border type

--- src/forceProcessors/ai/ant.ts
+++ src/forceProcessors/ai/ant.ts
@@ -25,6 +25,7 @@ const CHANCE_TO_CARRY_POINT = {
     [EPointType.StaticStone]: 0.04,
     [EPointType.Wood]: 0.08,
     [EPointType.Ant]: 0,
+    [EPointType.Border]: 0,
     default: 0.01,
 } as const
 
